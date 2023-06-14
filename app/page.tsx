c98224d22e1ae3358450d8ea4838e4eaad9d9d71
Chi <cheez2012@gmail.com>
2023-06-14 12:53:01 +0000

    i broke it. sad

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import Balancer from 'react-wrap-balancer';
-import { DEPLOY_URL } from '@/lib/constants';
-import { nFormatter } from '@/lib/utils';
-import InputWithLabel from '../components/InputWithLabel';
 
 export default function Home() {
   return (
@@ -17,31 +14,8 @@ export default function Home() {
           </Balancer>
         </p>
         <div className="mx-auto mt-6 flex animate-fade-up items-center justify-center space-x-5 opacity-0">
-          <a
-            className="group flex max-w-fit items-center justify-center space-x-2 rounded-full border border-black bg-black px-5 py-2 text-sm text-white transition-colors hover:bg-white hover:text-black"
-            href={DEPLOY_URL}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <svg
-              className="h-4 w-4 group-hover:text-black"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M12 4L20 20H4L12 4Z"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-            <p>Deploy to Vercel</p>
-          </a>
         </div>
       </div>
-      <InputWithLabel label="My Label" />
-    </>
+=    </>
   );
 }
